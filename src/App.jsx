c58bf90cef07e27807a5bd869fb98837ad4ea300
Coding-astronaut <129109894+Coding-astro@users.noreply.github.com>
2024-09-02 10:26:07 +0100

    Update App.jsx with new changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 
 import About from "./pages/About"
-import Home from "./pages/home"
+import Home from "./pages/Home"
 import Vans from "./pages/Vans"
 
-import Nav from "./components/nav"
+import Nav from "./components/Nav"
 import Footer from "./components/Footer"
 
 
